0988f4de1121d835a1fef053d8209b5440c854d2
Yu Shimura <mail@yuhr.org>
2021-07-23 00:19:26 +0900

    test: add checks for render count

--- src/index.test.ts
+++ src/index.test.ts
@@ -25,48 +25,61 @@ describe("useAwaitData", () => {
   })
 
   it("should handle fulfillments", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const result = useAwaitData(async () => {
         return await wait(2000)
       })
+      render()
       return { result }
     })
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(2000)
     expect(result.current.result.status).toBe("running")
+    expect(render).toHaveBeenCalledTimes(1)
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("fulfilled")
+    expect(render).toHaveBeenCalledTimes(2)
   })
 
   it("should handle rejections", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const result = useAwaitData(async () => {
         throw await new Promise(resolve =>
           setTimeout(resolve, 2000, new Error()),
         )
       })
+      render()
       return { result }
     })
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(2000)
     expect(result.current.result.status).toBe("running")
+    expect(render).toHaveBeenCalledTimes(1)
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("rejected")
+    expect(render).toHaveBeenCalledTimes(2)
   })
 
   it("should handle dependencies update before the first run settles", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const [state, setState] = useState({})
       const update = useCallback(() => setState({}), [])
       const result = useAwaitData(async () => {
         return await wait(2000)
       }, [state])
+      render()
       return { result, update } as const
     })
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
+    expect(render).toHaveBeenCalledTimes(1)
     act(result.current.update)
+    // State change + result update (for the new abort function)
+    expect(render).toHaveBeenCalledTimes(3)
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
@@ -74,15 +87,18 @@ describe("useAwaitData", () => {
     expect(result.current.result.status).toBe("running")
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("fulfilled")
+    expect(render).toHaveBeenCalledTimes(4)
   })
 
   it("should handle dependencies update after the first run settled", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const [state, setState] = useState({})
       const update = useCallback(() => setState({}), [])
       const result = useAwaitData(async () => {
         return await wait(2000)
       }, [state])
+      render()
       return { result, update } as const
     })
     expect(result.current.result.status).toBe("running")
@@ -90,9 +106,12 @@ describe("useAwaitData", () => {
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
+    expect(render).toHaveBeenCalledTimes(1)
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("fulfilled")
+    expect(render).toHaveBeenCalledTimes(2)
     act(result.current.update)
+    expect(render).toHaveBeenCalledTimes(4)
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
@@ -100,20 +119,24 @@ describe("useAwaitData", () => {
     expect(result.current.result.status).toBe("running")
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("fulfilled")
+    expect(render).toHaveBeenCalledTimes(5)
   })
 
   it("should handle dependencies update after the first run aborted but tick isn't used", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const [state, setState] = useState({})
       const update = useCallback(() => setState({}), [])
       const result = useAwaitData(async () => {
         return await wait(2000)
       }, [state])
+      render()
       return { result, update } as const
     })
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
+    expect(render).toHaveBeenCalledTimes(1)
     act(() => {
       result.current.result.status === "running" &&
         result.current.result.abort()
@@ -123,7 +146,9 @@ describe("useAwaitData", () => {
     expect(result.current.result.status).toBe("aborted")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("aborted")
+    expect(render).toHaveBeenCalledTimes(2)
     act(result.current.update)
+    expect(render).toHaveBeenCalledTimes(4)
     expect(result.current.result.status).toBe("running")
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
@@ -131,9 +156,11 @@ describe("useAwaitData", () => {
     expect(result.current.result.status).toBe("running")
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("fulfilled")
+    expect(render).toHaveBeenCalledTimes(5)
   })
 
   it("should abort `AbortController` at abort request", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const [state, setState] = useState({})
       const [isAborted, setIsAborted] = useState(false)
@@ -145,6 +172,7 @@ describe("useAwaitData", () => {
         },
         [state],
       )
+      render()
       return { result, update, isAborted } as const
     })
     expect(result.current.result.status).toBe("running")
@@ -152,15 +180,18 @@ describe("useAwaitData", () => {
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
     expect(result.current.isAborted).toBe(false)
+    expect(render).toHaveBeenCalledTimes(1)
     act(() => {
       result.current.result.status === "running" &&
         result.current.result.abort()
     })
     expect(result.current.result.status).toBe("aborted")
     expect(result.current.isAborted).toBe(true)
+    expect(render).toHaveBeenCalledTimes(2)
   })
 
   it("should abort `AbortController` at invalidate", async () => {
+    const render = jest.fn()
     const { result, waitForNextUpdate } = renderHook(() => {
       const [state, setState] = useState({})
       const [isAborted, setIsAborted] = useState(false)
@@ -172,6 +203,7 @@ describe("useAwaitData", () => {
         },
         [state],
       )
+      render()
       return { result, update, isAborted } as const
     })
     expect(result.current.result.status).toBe("running")
@@ -179,7 +211,9 @@ describe("useAwaitData", () => {
     jest.advanceTimersByTime(1000)
     expect(result.current.result.status).toBe("running")
     expect(result.current.isAborted).toBe(false)
+    expect(render).toHaveBeenCalledTimes(1)
     act(result.current.update)
+    expect(render).toHaveBeenCalledTimes(3)
     expect(result.current.result.status).toBe("running")
     expect(result.current.isAborted).toBe(true)
     jest.advanceTimersByTime(1000)
@@ -190,5 +224,6 @@ describe("useAwaitData", () => {
     expect(result.current.isAborted).toBe(true)
     await waitForNextUpdate()
     expect(result.current.result.status).toBe("fulfilled")
+    expect(render).toHaveBeenCalledTimes(4)
   })
 })
